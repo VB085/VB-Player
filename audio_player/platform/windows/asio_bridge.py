29dde57f76753b93a8529a1f7f13de8d8feecb48
vb085 <vb085@users.noreply.github.com>
2026-06-24 23:29:54 +0800

    fix: asio_bridge.py缺少import sys

--- audio_player/platform/windows/asio_bridge.py
+++ audio_player/platform/windows/asio_bridge.py
@@ -7,6 +7,7 @@ The engine_windows module expects module-level functions and globals:
     _a._wpos, _a._rpos, _a._ch, _a._bs, _a._running, _a.RING_SAMPLES
 """
 import asio_ctypes
+import sys
 
 _dev = None  # current ASIODevice instance
 
